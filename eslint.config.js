f61e065800575a842162d8c21ebae44fe73aea95
lucasmoraist <luksmnt1101@gmail.com>
2024-09-06 19:39:20 -0300

    Update eslint.config.js to enforce single quotes and semicolons

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
     },
   },
 )
