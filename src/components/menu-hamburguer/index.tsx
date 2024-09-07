a8fb0b9bd6e0472d0e297d562e32c2a697c61211
lucasmoraist <luksmnt1101@gmail.com>
2024-09-07 15:23:34 -0300

    feat: created menu component

---
+++ src/components/menu-hamburguer/index.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import style from './menu.module.scss';
+import { Menu, X } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { Button } from '../button';
+
+interface Props {
+  isLogged: boolean;
+}
+
+export function MenuHamburguer({ isLogged }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={style.menuHamburguer}>
+      <nav>
+        <Menu
+          className={`${style.burguerBar} ${
+            isOpen ? style.clicked : style.unclicked
+          }`}
+          onClick={toggleMenu}
+        />
+      </nav>
+
+      <div
+        className={`${style.overlay} ${isOpen ? style.visible : style.hidden}`}
+        onClick={toggleMenu}
+      ></div>
+
+      <div className={`${style.menu} ${isOpen ? style.visible : style.hidden}`}>
+        <div className={style.closeButton} onClick={toggleMenu}>
+          <X />
+        </div>
+        <ul className={`${!isLogged && style.disconnected}`}>
+          {isLogged ? (
+            <div className={style.links}>
+              <div>
+                <Link className={style.linkNavigate} to={'/signup'}>
+                  Create Post
+                </Link>
+                <Link className={style.linkNavigate} to={'/profile'}>
+                  Profile
+                </Link>
+              </div>
+              <Link className={style.linkNavigate} to={'/logout'}>
+                Logout
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Button option="secondary">Sign-up</Button>
+              <Button option="primary">Sign-in</Button>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
