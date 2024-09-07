fee1879979a20edc1aa467a9d26a2ca555763c0d
lucasmoraist <luksmnt1101@gmail.com>
2024-09-07 19:39:43 -0300

    refactor: Update link text for creating a post in header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -24,7 +24,7 @@ export function Header({ isLogged }: Props) {
           <div className={style.links}>
             <div>
               <Link className={style.linkNavigate} to={'/signup'}>
-                Create Post
+                Write
               </Link>
               <Link className={style.linkNavigate} to={'/profile'}>
                 Profile
