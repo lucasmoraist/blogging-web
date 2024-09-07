b1e8f82b307ce7b9aa169eced07b3a2fcf860798
lucasmoraist <luksmnt1101@gmail.com>
2024-09-07 15:23:00 -0300

    feat: created footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import style from './footer.module.scss'
+
+export function Footer() {
+    return (
+        <footer className={style.footer}>
+            <p>&copy; Desenvolvido em 2024 • Grupo 8</p>
+        </footer>
+    )
+}
