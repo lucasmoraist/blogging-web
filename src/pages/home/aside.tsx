7cf5532b3b4dbb058f4bc7c1129c2642d144d88b
lucasmoraist <luksmnt1101@gmail.com>
2024-09-10 09:24:30 -0300

    feat: Add aside component to display random posts in the home page

---
+++ src/pages/home/aside.tsx
@@ -0,0 +1,36 @@
+import { IPost } from '@/interface/post.interface';
+import style from './styles/aside.module.scss';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  posts: IPost[];
+}
+
+// Idéia:
+// De início os posts que irão aparecer na lateral podem ser apenas posts aleatórios
+// Mas pode-se mudar o objetivo e mostrar apenas os posts melhores avaliados
+export function Aside({ posts }: Props) {
+  const [randomPosts, setRandomPosts] = useState<IPost[]>([]);
+
+  const getRandomPosts = (postList: IPost[], count: number): IPost[] => {
+    return postList.sort(() => Math.random() - 0.5).slice(0, count);
+  };
+
+  useEffect(() => {
+    const selectedPosts = getRandomPosts(posts, 4);
+    setRandomPosts(selectedPosts);
+  }, []);
+
+  return (
+    <aside className={style.asideContainer}>
+      <div className={style.postWrapper}>
+        {randomPosts.map((post) => (
+          <div key={post.id} className={style.post}>
+            <img src={post.image} alt={`Imagem do post sobre ${post.title}`} width={200} />
+            <h3>{post.title}</h3>
+          </div>
+        ))}
+      </div>
+    </aside>
+  );
+}
