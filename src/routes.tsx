e5bbdd8c9bc982b2b5ae0cb0b65cbd348acaa43f
lucasmoraist <luksmnt1101@gmail.com>
2024-09-10 10:03:38 -0300

    feat: Add route for post details page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Home } from './pages/home';
 import { Header } from './components/header';
 import { useState } from 'react';
 import { Footer } from './components/footer';
+import { PostDetails } from './pages/postDetails';
 
 export function AppRoutes() {
     const [isLogged, setIsLogged] = useState(false);
@@ -11,6 +12,7 @@ export function AppRoutes() {
             <Header isLogged={isLogged} />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path='/post/:id' element={<PostDetails />}/>
             </Routes>
             <Footer />
         </Router>
