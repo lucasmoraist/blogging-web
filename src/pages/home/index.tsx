3ea89dffc0855ee8d10c52eaee5def91d13dd5aa
lucasmoraist <luksmnt1101@gmail.com>
2024-09-10 09:27:56 -0300

    refactor: Update home module styles and add aside component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,21 @@
 import { Post } from '@/components/post';
 import posts from '@/data/posts.json';
-import style from './home.module.scss';
+import style from './styles/home.module.scss';
+import { Aside } from './aside';
 
 export function Home() {
-    return <div className={style.homeContainer}>
-        {posts.slice(0, 5).map(post => (
-            <div key={post.id}>
-                <Post post={post}/>
-            </div>
+  return (
+    <div className={style.homeContainer}>
+      <div className={style.feed}>
+        {posts.slice(0, 5).map((post) => (
+          <div key={post.id}>
+            <Post post={post} />
+          </div>
         ))}
+      </div>
+      <div className={style.randomPosts}>
+        <Aside posts={posts}/>
+      </div>
     </div>
+  );
 }
