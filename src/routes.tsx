a4070736ed4e65e65256c1f3410bbcd2ba26e5b8
lucasmoraist <luksmnt1101@gmail.com>
2024-09-12 13:08:16 -0300

    Refactor routes.tsx to add registration and login routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
-import { useState } from 'react';
 import { PostDetails } from './pages/postDetails';
 import { PageDefault } from './components/pageDefault';
 import { NotFound } from './pages/notFound';
+import { Register } from './pages/auth/register';
+import { Login } from './pages/auth/login';
 
 export function AppRoutes() {
-  const [isLogged, setIsLogged] = useState(false);
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<PageDefault isLogged={isLogged} />}>
+          <Route path="/" element={<PageDefault />}>
             <Route index element={<Home />} />
             <Route path="post/:postId" element={<PostDetails />} />
           </Route>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/login' element={<Login />}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
