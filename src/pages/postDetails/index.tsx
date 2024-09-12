00f6050a6c2aa91f341a6215e7011730b91201e6
lucasmoraist <luksmnt1101@gmail.com>
2024-09-12 10:51:11 -0300

    Refactor postDetails component to fetch and display a single post from API

--- src/pages/postDetails/index.tsx
+++ src/pages/postDetails/index.tsx
@@ -1,27 +1,39 @@
-import { PostRender } from "@/components/postRender";
-import { useNavigate, useParams } from "react-router-dom"
-import posts from "@/data/posts.json";
-import style from './post-details.module.scss'
-import { ArrowLeft } from "lucide-react";
-import { NotFound } from "../notFound";
+import { PostRender } from '@/components/postRender';
+import { useNavigate, useParams } from 'react-router-dom';
+import style from './post-details.module.scss';
+import { ArrowLeft } from 'lucide-react';
+import { NotFound } from '../notFound';
+import { IPost } from '@/interface/post.interface';
+import { useEffect, useState } from 'react';
+import { http } from '@/utils/axios';
 
 export function PostDetails() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const { id } = useParams();
-    const postId = Number(id);
+  const [posts, setPosts] = useState<IPost>();
 
-    const post = posts.find((post) => post.id === postId);
+  const { postId } = useParams();
 
-    if (!post) return  <NotFound />
+  useEffect(() => {
+    http()
+      .get(`/posts/${postId}`)
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [postId]);
 
-    return (
-        <main className={style.mainContainer}>
-            <button onClick={() => navigate('/')}>
-                <ArrowLeft />
-                Voltar
-            </button>
-            <PostRender id={postId} post={post} />
-        </main>
-    )
+  if (!posts) return <NotFound />;
+
+  return (
+    <main className={style.mainContainer}>
+      <button onClick={() => navigate('/')}>
+        <ArrowLeft />
+        Voltar
+      </button>
+      <PostRender id={postId} post={posts} />
+    </main>
+  );
 }
