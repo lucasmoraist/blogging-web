f3bd25a5e3fc19cca897e4cec537ff18d85177b3
lucasmoraist <luksmnt1101@gmail.com>
2024-09-12 10:50:18 -0300

    refactor: Update Home component to fetch and display posts from API

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,38 @@
 import { PostRender } from '@/components/postRender';
-import posts from '@/data/posts.json';
 import style from './styles/home.module.scss';
 import { Aside } from './aside';
+import { useEffect, useState } from 'react';
+import { IPost } from '@/interface/post.interface';
+import { http } from '@/utils/axios';
+import { SearchForm } from './search';
 
 export function Home() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    http()
+      .get('/posts')
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className={style.homeContainer}>
-      <div className={style.feed}>
-        {posts.slice(0, 5).map((post) => (
-          <div key={post.id}>
-            <PostRender post={post} />
-          </div>
-        ))}
+      <div className={style.homeContainer}>
+        <div className={style.feed}>
+        <SearchForm />
+          {posts.slice(0, 5).map((post) => (
+            <div key={post.id}>
+              <PostRender post={post} />
+            </div>
+          ))}
+        </div>
+        <div className={style.randomPosts}>
+          <Aside posts={posts} />
+        </div>
       </div>
-      <div className={style.randomPosts}>
-        <Aside posts={posts}/>
-      </div>
-    </div>
   );
 }
