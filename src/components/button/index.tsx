3854bf2a01b512db425757e731b066f2232c5bc9
lucasmoraist <luksmnt1101@gmail.com>
2024-09-12 15:31:46 -0300

    Refactor button component to include optional 'type' prop

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,15 +4,16 @@ interface Props {
     children: React.ReactNode;
     option: 'primary' | 'secondary';
     onClick?: () => void;
+    type?: 'button' | 'submit' | 'reset';
 }
 
-export function Button({ children, option, onClick }: Props) {
+export function Button({ children, option, onClick, type }: Props) {
     return (
         <>
             {option === 'primary' ? (
-                <button className={`${style.btn} ${style.btnPrimary}`} onClick={onClick}>{children}</button>
+                <button className={`${style.btn} ${style.btnPrimary}`} type={type} onClick={onClick}>{children}</button>
             ) : (
-                <button className={`${style.btn} ${style.btnSecondary}`} onClick={onClick}>{children}</button>
+                <button className={`${style.btn} ${style.btnSecondary}`} type={type} onClick={onClick}>{children}</button>
             )}
         </>
     );
