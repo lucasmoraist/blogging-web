c2fd9edf00e78852f8aeb768825d79dc0f8116da
lucasmoraist <luksmnt1101@gmail.com>
2024-09-13 10:56:08 -0300

    refactor: Update axios configuration to include authorization header

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
+import { redirect } from "react-router-dom";
 
 export function http() {
+    const token = localStorage.getItem('token');
+    if (!token) redirect('/login');
+
     return axios.create({
         baseURL: 'http://localhost:3000',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     })
 }
