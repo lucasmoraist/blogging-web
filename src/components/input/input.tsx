50d1e0b37d3b4e8f9fe987217c361932b2c7d491
lucasmoraist <luksmnt1101@gmail.com>
2024-09-13 12:59:32 -0300

    refactor: Update Input component to include placeholder prop

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -5,13 +5,14 @@ interface Props {
   title: string;
   name: string;
   type: string;
+  placeholder?: string;
 }
 
-export function Input({ title, name, type }: Props) {
+export function Input({ title, name, type, placeholder }: Props) {
   return (
     <div className={style.inputComponent}>
       <span>{title}</span>
-      <Field type={type} name={name} required/>
+      <Field type={type} name={name} placeholder={placeholder} required/>
       <ErrorMessage name={name}>
         {(msg) => (
           <div style={{ marginTop: '4px', width: '260px' }}>
