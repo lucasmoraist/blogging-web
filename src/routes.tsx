5082a8dc07a9aa52726c745368c213087d35db9a
lucasmoraist <luksmnt1101@gmail.com>
2024-09-16 10:12:36 -0300

    refactor: Update routes.tsx to use default import for PageDefault component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
 import { PostDetails } from './pages/postDetails';
-import { PageDefault } from './components/pageDefault';
+import PageDefault from './components/pageDefault';
 import { NotFound } from './pages/notFound';
 import { Register } from './pages/auth/register';
 import { Login } from './pages/auth/login';
@@ -11,25 +11,23 @@ import { FormPost } from './pages/createPost';
 
 export function AppRoutes() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<PageDefault />}>
-            <Route index element={<Home />} />
-            <Route path="post/:postId" element={<PostDetails />} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<PageDefault />}>
+          <Route index element={<Home />} />
+          <Route path="post/:postId" element={<PostDetails />} />
 
-            <Route element={<PrivateRoute />}>
-              <Route path='/admin/posts' element={<ListPosts />}/>
-              <Route path='/admin/create' element={<FormPost />}/>
-              <Route path='/admin/update/:id' element={<FormPost />}/>
-              {/* Requisição de exclusão será apenas um botão para excluir */}
-            </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/admin/posts" element={<ListPosts />} />
+            <Route path="/admin/create" element={<FormPost />} />
+            <Route path="/admin/update/:id" element={<FormPost />} />
+            {/* Requisição de exclusão será apenas um botão para excluir */}
           </Route>
-          <Route path='/register' element={<Register />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
