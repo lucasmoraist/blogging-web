1d2720190a368026d13cfb9e68413442fe0c045d
lucasmoraist <luksmnt1101@gmail.com>
2024-09-21 23:07:59 -0300

    refactor: Update axios baseURL to use environment variable

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,10 +3,11 @@ import { redirect } from "react-router-dom";
 
 export function http() {
     const token = localStorage.getItem('token');
+    const prodUrl = process.env.REACT_APP_API_URL;
     if (!token) redirect('/login');
 
     return axios.create({
-        baseURL: 'http://localhost:3000',
+        baseURL: prodUrl || 'http://localhost:3000',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
