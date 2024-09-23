0904197b2945ec0b57eefe2006b389030e052848
lucasmoraist <luksmnt1101@gmail.com>
2024-09-23 08:18:06 -0300

    Refactor PostsContainer component to fetch and display posts dynamically

--- src/pages/home-2/posts-container/posts-container.tsx
+++ src/pages/home-2/posts-container/posts-container.tsx
@@ -1,34 +1,83 @@
 import styled from 'styled-components';
 import PostCard from './posts-card';
-import strings from '../scripts/strings';
 import { IPost } from '@/interface/post.interface';
 import { schoolSubjects } from '../scripts/constants';
+import { useEffect, useState } from 'react';
+import { http } from '@/utils/axios';
 
-interface Props {
+interface DataPagination {
+  currentPage: number;
+  itemsPerPage: number;
   posts: IPost[];
+  totalNumberOfPages: number;
 }
 
+const PostsContainer = (): JSX.Element => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [currentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
-const PostsContainer = ({ posts }: Props): JSX.Element => {
-    return (
-      <StyledPostsContainer>
+  const fetchPosts = async (signal: AbortSignal) => {
+    try {
+      const response = await http().get<DataPagination>(
+        `/posts?page=${currentPage}&limit=${itemsPerPage}`,
+        { signal }
+      );
+      return response.data.posts;
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.name === 'AbortError') {
+          console.log('Request canceled');
+        } else {
+          throw new Error('Error when searching for posts: ' + error.message);
+        }
+      } else {
+        throw new Error('Unexpected error');
+      }
+    }
+  };
 
-        {schoolSubjects.map((subject) => ( 
-          <>
+  useEffect(() => {
+    const controller = new AbortController();
+    fetchPosts(controller.signal)
+      .then((data) => {
+        if (data) setPosts(data);
+        setLoading(true);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      })
+      .finally(() => setLoading(false));
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <StyledPostsContainer>
+      {schoolSubjects.map((subject) => (
+        <>
           <StyledSubjectName>{subject.label}</StyledSubjectName>
           <StyledSubjectSubtitle>{subject.description}</StyledSubjectSubtitle>
-          </>
-        ))}
+        </>
+      ))}
       <StyledPostsContainerUl>
-          {posts.length > 0 && posts.map((item, index) => (
+        {loading ? <p>loading...</p> : posts.length > 0 &&
+          posts.map((item, index) => (
             <li key={index}>
-              <PostCard
-                post={item} />
+              <PostCard post={item} />
             </li>
-          ))}
-        </StyledPostsContainerUl>
-        </StyledPostsContainer>
-    );
+          )) }
+      </StyledPostsContainerUl>
+      <div>
+        {loading ? <p>loading...</p> : error ? <p>{error}</p> : null}
+      </div>
+    </StyledPostsContainer>
+  );
 };
 
 export default PostsContainer;
@@ -36,7 +85,7 @@ export default PostsContainer;
 const StyledPostsContainer = styled.div`
   font-family: Helvetica, sans-serif;
   width: 75%;
-  background-color: #ECF0F1;
+  background-color: #ecf0f1;
   padding: 0 16px 16px 16px;
   height: fit-content;
   border-radius: 12px;
@@ -48,20 +97,20 @@ const StyledPostsContainer = styled.div`
 `;
 
 const StyledSubjectName = styled.p`
-    font-size: 1.5rem;
-    font-weight: bold;
-    display: inline-flex;
-    margin: 0;
-    padding-left: 16px;
-    color: #e36414;
+  font-size: 1.5rem;
+  font-weight: bold;
+  display: inline-flex;
+  margin: 0;
+  padding-left: 16px;
+  color: #e36414;
 `;
 
 const StyledSubjectSubtitle = styled.p`
-    font-size: 0.875rem;
-    margin: 0;
-    padding-left: 16px;
-    color: #219ebc;
-    padding-top: 8px;
+  font-size: 0.875rem;
+  margin: 0;
+  padding-left: 16px;
+  color: #219ebc;
+  padding-top: 8px;
 `;
 
 const StyledPostsContainerUl = styled.ul`
