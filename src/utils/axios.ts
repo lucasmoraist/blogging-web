ccd55aff85c7e58442791e07b7f13b5b4424efe0
lucasmoraist <luksmnt1101@gmail.com>
2024-09-23 07:53:47 -0300

    Refactor axios configuration to use dynamic API URL based on environment

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,11 +3,12 @@ import { redirect } from 'react-router-dom';
 
 export function http() {
     const token = localStorage.getItem('token');
+    const profile = import.meta.env.VITE_PROFILE;
+    const apiUrl = profile === 'dev' ? 'http://localhost:3000' : import.meta.env.VITE_API_URL;
     if (!token) redirect('/login');
 
     return axios.create({
-        // baseURL: 'http://localhost:3000/',
-        baseURL: 'https://tech-challenge2.grupo8.tech/',
+        baseURL: apiUrl,
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
