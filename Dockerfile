7caa55de0f369b653add03b8d7749aaa96084deb
lucasmoraist <luksmnt1101@gmail.com>
2024-09-23 13:41:14 -0300

    refactor: Remove unused VITE_PROFILE variable and update Dockerfile and axios.ts to use VITE_API_URL

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,9 @@ RUN npm install
 
 COPY . .
 
-ARG VITE_PROFILE=$VITE_PROFILE
 ARG VITE_API_URL=$VITE_API_URL
 
-RUN echo "VITE_PROFILE=${VITE_PROFILE}" >> .env && \
-    echo "VITE_API_URL=${VITE_API_URL}" >> .env
+RUN echo "VITE_API_URL=${VITE_API_URL}" >> .env
 
 RUN npm run build
 
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,8 +3,7 @@ import { redirect } from 'react-router-dom';
 
 export function http() {
     const token = localStorage.getItem('token');
-    const profile = import.meta.env.VITE_PROFILE;
-    const apiUrl = profile === 'dev' ? 'http://localhost:3000' : import.meta.env.VITE_API_URL;
+    const apiUrl = import.meta.env.VITE_API_URL;
     if (!token) redirect('/login');
 
     return axios.create({
