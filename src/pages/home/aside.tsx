a58ad880bf664a731702f7224bbb1c00b8c04291
lucasmoraist <luksmnt1101@gmail.com>
2024-09-24 15:35:01 -0300

    refactor: Update post card styling and layout

--- src/pages/home/aside.tsx
+++ src/pages/home/aside.tsx
@@ -1,7 +1,7 @@
-import { IPost } from '@/interface/post.interface';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { IPost } from "@/interface/post.interface";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 interface Props {
   posts: IPost[];
@@ -25,17 +25,15 @@ export function Aside({ posts }: Props): JSX.Element {
 
   const trimText = (text: string, maxLength: number = 160): string => {
     if (text.length > maxLength) {
-      return text.slice(0, maxLength) + '...';
+      return text.slice(0, maxLength) + "...";
     }
     return text;
   };
 
   return (
     <StyledLatestPostsContainer>
-      <StyledLatestPostsTitle>
-        Nossos últimos posts!
-      </StyledLatestPostsTitle>
       <StyledPostWrapper>
+        <StyledLatestPostsTitle>Nossos últimos posts!</StyledLatestPostsTitle>
         {randomPosts.map((post, index) => (
           <StyledPostCard
             key={index}
@@ -62,23 +60,23 @@ const StyledLatestPostsContainer = styled.div`
 const StyledPostWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
   gap: 16px;
 `;
 
 const StyledPostCard = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 16px 0 16px 0;
   padding: 16px;
-  width: 264px;
+  width: 350px;
   background-color: #d3edf5;
   border-radius: 8px;
   box-shadow: 0px 1px 2px rgba(47, 46, 46, 0.3);
   position: relative;
   display: flex;
-  gap: 15px;
+  gap: 30px;
   cursor: pointer;
-  height: 160px;
 `;
 
 const StyledPostCardTitle = styled.a`
@@ -91,7 +89,6 @@ const StyledPostCardTitle = styled.a`
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: clip;
-  width: 240px;
   cursor: pointer;
   margin: 0;
   font-family: Helvetica, sans-serif;
@@ -101,19 +98,14 @@ const StyledPostContent = styled.div`
   font-size: 0.875rem;
   font-weight: 400;
   text-align: left;
-  width: 230px;
   margin-top: 0px;
   cursor: pointer;
   padding: 2px;
   font-family: Helvetica, sans-serif;
 `;
- 
- const StyledLatestPostsTitle = styled.p`
+
+const StyledLatestPostsTitle = styled.p`
   font-weight: 600;
   color: #e36414;
-  display: flex;
-  text-align: center;
-  align-self: self-end;
   font-family: Helvetica, sans-serif;
-  margin-left: 36px;
- `;
+`;
